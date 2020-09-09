be8732fcdc4a73babe6612bcb3d5e45ed34ea684
Orhan Yarar <orhan@hotmail.ca>
2020-09-09 16:31:18 -0400

    new test added

--- test/index.test.js
+++ test/index.test.js
@@ -5,4 +5,8 @@ describe("adder Test", function () {
   it("1+1 should return 2", function () {
     expect(adder(1, 1)).equal(2);
   });
+
+  it("2+1 should return 3", function () {
+    expect(adder(2, 1)).equal(3);
+  });
 });
